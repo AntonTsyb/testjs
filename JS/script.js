6d53878cc94b9ffd487fb1c4de1ac02f758ef2d9
Anton <antontsyb@gmail.com>
2019-02-13 21:34:03 +0200

    Initial folder

---
+++ JS/script.js
@@ -0,0 +1,51 @@
+// console.log(4 / 0);
+// console.log('int' * 4);
+
+// let something;
+// console.log(something);
+
+// let persone = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(persone["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+// console.log(arr[0]);
+
+// // alert("Hello, world");
+
+// // let answer = confirm("Are you here?");
+
+// // console.log(answer);
+
+// // let answer = +prompt("Есть ли Вам 18?", "");
+
+// // console.log(typeof (answer));
+
+// console.log("arr" + " - object");
+// console.log(4 + +"2");
+'use strict';
+
+let a = 5;
+
+console.log(a);
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(incr);
+console.log(decr);
+
+console.log(5 % 2);
+console.log("2" === 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);    
